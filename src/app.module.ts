988c3ad7c1dac14517d59279af7ad0811018eb10
caubebutchii <caubebutchi2k3@gmail.com>
2025-02-24 20:29:04 +0700

    init prisma client

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnrollmentController } from './enrollment/enrollment.controller';
 import { EnrollmentService } from './enrollment/enrollment.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController, EnrollmentController],
   providers: [AppService, EnrollmentService],
 })

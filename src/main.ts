d5dc6fb08fefca1e78b19a44b2ab80632b33bea5
caubebutchii <caubebutchi2k3@gmail.com>
2025-04-17 10:36:45 +0700

    fix cấu hình cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://eduforge.io.vn'],
+    origin: ['http://localhost:3000', 'https://eduforge.io.vn', "https://kong.eduforge.io.vn"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization', 'X-User-Id'],
     credentials: true,
